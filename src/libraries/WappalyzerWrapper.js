7534b0abe397c3bcbf86e3be5126929cb6287e5a
Akrobate <fedorov.artiom@gmail.com>
2020-12-09 13:02:45 +0100

    Added full process static method / work in progress should be refactored

---
+++ src/libraries/WappalyzerWrapper.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const Wappalyzer = require('wappalyzer');
+
+class WappalyzerWrapper {
+
+    // eslint-disable-next-line require-jsdoc
+    static get OPTIONS() {
+        return {
+            debug: false,
+            delay: 500,
+            headers: {},
+            htmlMaxCols: 2000,
+            htmlMaxRows: 2000,
+            maxDepth: 3,
+            maxUrls: 10,
+            maxWait: 5000,
+            probe: true,
+            recursive: true,
+            userAgent: 'Wappalyzer',
+        };
+    }
+
+    // eslint-disable-next-line require-jsdoc
+    static get HEADERS() {
+        return {};
+    }
+
+    // eslint-disable-next-line require-jsdoc
+    constructor() {
+        this.wappalyzer_instance = new Wappalyzer(WappalyzerWrapper.OPTIONS);
+    }
+
+    /**
+     * @returns {WappalyzerWrapper}
+     */
+    static getInstance() {
+        if (WappalyzerWrapper.instance === null) {
+            WappalyzerWrapper.instance = new WappalyzerWrapper();
+        }
+        return WappalyzerWrapper.instance;
+    }
+
+    /**
+     * @returns {Promise}
+     */
+    init() {
+        return this.wappalyzer_instance.init();
+    }
+
+    /**
+     * @param {String} url
+     * @returns {Promise}
+     */
+    analyse(url) {
+        const site = this.wappalyzer_instance
+            .open(url, WappalyzerWrapper.HEADERS);
+        // Optionally capture and output errors
+        // site.on('error', console.error)
+        return site.analyze();
+    }
+
+    /**
+     * @param {String} url
+     * @returns {Promise}
+     */
+    static initAnalyseDestroy(url) {
+        let wappalyzer = new Wappalyzer(WappalyzerWrapper.OPTIONS);
+        return wappalyzer
+            .init()
+            .then(() => wappalyzer.open(url, WappalyzerWrapper.HEADERS))
+            .then((site) => site.analyze())
+            .then((result) => wappalyzer.destroy()
+                .then(() => {
+                    wappalyzer = null;
+                    return result;
+                })
+            );
+    }
+
+    /**
+     * @returns {Promise}
+     */
+    destroy() {
+        return this.wappalyzer_instance.destroy();
+    }
+}
+
+WappalyzerWrapper.instance = null;
+
+module.exports = {
+    WappalyzerWrapper,
+};
