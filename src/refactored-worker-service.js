f8ccd1e4ef742514678e11d6cc09f49185b96c03
Akrobate <fedorov.artiom@gmail.com>
2020-12-11 23:37:59 +0100

    With timeout worker implementation

--- src/refactored-worker-service.js
+++ src/refactored-worker-service.js
@@ -2,6 +2,7 @@
 
 const {
     AmqpQueueBuilder,
+    WappalyzerWrapper,
 } = require('./libraries');
 
 const {
@@ -10,14 +11,68 @@ const {
 
 const amqp_queue = AmqpQueueBuilder.getInstance(configuration.rabbitmq);
 
+// Rewrite to promize all instead of this promise hell
 amqp_queue
-    .createOrUseQueue(configuration.queue.worker_queue.name, 1)
-    .then((worker_queue) => amqp_queue.createOrUseQueue(configuration.queue.result_queue.name)
+    .createOrUseQueue(configuration.queue.worker_queue.name, 5)
+    .then((worker_queue) => amqp_queue
+        .createOrUseQueue(configuration.queue.result_queue.name)
         .then((result_queue) => {
             // eslint-disable-next-line arrow-body-style
             amqp_queue.consume(worker_queue, (data) => {
-                // console.log(data);
-                return amqp_queue.publish(result_queue, data);
+                const {
+                    website_url,
+                } = data;
+
+                console.log('Consuming - ' + data.id + ' url ' + data.website_url);
+                return WappalyzerWrapper
+                    .initAnalyseDestroyWithTimeout(website_url)
+                    .catch((error) => ({
+                        error,
+                    }))
+                    .then((wappalyser_data) => amqp_queue
+                        .publish(result_queue, Object.assign(
+                            {},
+                            data,
+                            {
+                                wappalyser_data: JSON.stringify(wappalyser_data),
+                            }
+                        ))
+                    );
             });
-        })
-    );
+        }));
+
+/*
+wappalyzer_wrapper
+    .init()
+    .then(() => {
+        amqp_queue
+            .createOrUseQueue(configuration.queue.worker_queue.name, 10)
+            .then((worker_queue) => amqp_queue
+                .createOrUseQueue(configuration.queue.result_queue.name)
+                .then((result_queue) => {
+                    // eslint-disable-next-line arrow-body-style
+                    amqp_queue.consume(worker_queue, (data) => {
+                        const {
+                            website_url,
+                        } = data;
+
+                        console.log('Consuming - ' + data.id + ' url ' + data.website_url);
+                        return wappalyzer_wrapper
+                            .analyse(website_url)
+                            .catch((error) => ({
+                                error,
+                            }))
+                            .then((wappalyser_data) => amqp_queue
+                                .publish(result_queue, Object.assign(
+                                    {},
+                                    data,
+                                    {
+                                        wappalyser_data: JSON.stringify(wappalyser_data),
+                                    }
+                                ))
+                            );
+                    });
+                }));
+    });
+
+*/
