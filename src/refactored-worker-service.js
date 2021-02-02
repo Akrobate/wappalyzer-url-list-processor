5f6bc8034f925ddebadc785ce01a7522b7584f9f
Akrobate <fedorov.artiom@gmail.com>
2021-02-02 12:53:35 +0100

    cleaned worker service file

--- src/refactored-worker-service.js
+++ src/refactored-worker-service.js
@@ -9,11 +9,16 @@ const {
     configuration,
 } = require('./configuration');
 
+const {
+    logger,
+} = require('./logger');
+
+const PRE_FETCH = 5;
+
 const amqp_queue = AmqpQueueBuilder.getInstance(configuration.rabbitmq);
 
-// Rewrite to promize all instead of this promise hell
 amqp_queue
-    .createOrUseQueue(configuration.queue.worker_queue.name, 5)
+    .createOrUseQueue(configuration.queue.worker_queue.name, PRE_FETCH)
     .then((worker_queue) => amqp_queue
         .createOrUseQueue(configuration.queue.result_queue.name)
         .then((result_queue) => {
@@ -23,7 +28,7 @@ amqp_queue
                     website_url,
                 } = data;
 
-                console.log('Consuming - ' + data.id + ' url ' + data.website_url);
+                logger.log(`Consuming - ${data.id} url ${data.website_url}`);
                 return WappalyzerWrapper
                     .initAnalyseDestroyWithTimeout(website_url)
                     .catch((error) => ({
@@ -40,39 +45,3 @@ amqp_queue
                     );
             });
         }));
-
-/*
-wappalyzer_wrapper
-    .init()
-    .then(() => {
-        amqp_queue
-            .createOrUseQueue(configuration.queue.worker_queue.name, 10)
-            .then((worker_queue) => amqp_queue
-                .createOrUseQueue(configuration.queue.result_queue.name)
-                .then((result_queue) => {
-                    // eslint-disable-next-line arrow-body-style
-                    amqp_queue.consume(worker_queue, (data) => {
-                        const {
-                            website_url,
-                        } = data;
-
-                        console.log('Consuming - ' + data.id + ' url ' + data.website_url);
-                        return wappalyzer_wrapper
-                            .analyse(website_url)
-                            .catch((error) => ({
-                                error,
-                            }))
-                            .then((wappalyser_data) => amqp_queue
-                                .publish(result_queue, Object.assign(
-                                    {},
-                                    data,
-                                    {
-                                        wappalyser_data: JSON.stringify(wappalyser_data),
-                                    }
-                                ))
-                            );
-                    });
-                }));
-    });
-
-*/
